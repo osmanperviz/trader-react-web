1397e5b3ec35fea2f6d95ec7f5aaf2414ac4670d
Osman Perviz <osman.perviz@studitemps.de>
2022-05-29 13:12:53 +0200

    Add reate router and first default route.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes, Navigate } from "react-router-dom";
+import routes from "./routes";
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {getRoutes(routes)}
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+    
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import Icon from "@mui/material/Icon";
+
+
+const routes = [
+    {
+        type: "collapse",
+        name: "Dashboard",
+        key: "dashboard",
+        icon: <Icon fontSize="small">dashboard</Icon>,
+        route: "/dashboard",
+        component: <>MY ass!</>,
+    }
+]
+
+export default routes;
+
+
